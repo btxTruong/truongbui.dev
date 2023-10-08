0d2c08b98b3473c3f857733a798c06bf458eaaa0
Truong Bui <50477534+btxTruong@users.noreply.github.com>
2023-10-08 15:03:10 +0700

    chore: move to use webc

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 const dev = process.env.NODE_ENV === 'development';
 
-const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const vitePlugin = require('@11ty/eleventy-plugin-vite');
+const syntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight');
+const webcPlugin = require('@11ty/eleventy-plugin-webc');
 
 /** @type {import('@11ty/eleventy').UserConfig} */
 module.exports = function(eleventyConfig) {
 	// Plugins
-	eleventyConfig.addPlugin(EleventyVitePlugin);
-	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(vitePlugin);
+	eleventyConfig.addPlugin(syntaxHighlightPlugin);
+	eleventyConfig.addPlugin(webcPlugin, {
+		components: 'src/_includes/components/**/*.webc'
+	});
 
 	// Static assets
 	eleventyConfig.addPassthroughCopy('./src/public');
