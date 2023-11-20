50bdb07ac846ff3f3c9044dcd32e9fb880b19135
Truong Bui <50477534+btxTruong@users.noreply.github.com>
2023-11-20 21:07:52 +0700

    feat: add docsearch

---
+++ src/assets/js/docsearch.js
@@ -0,0 +1,12 @@
+import docsearch from '@docsearch/js';
+
+import '@docsearch/css';
+
+docsearch({
+	container: '#docsearch',
+	appId: 'W7XZTS6NJN',
+	indexName: 'truongbui',
+	apiKey: 'b4e7356ad43208a8bbc55eaa79bab524',
+	insights: true,
+	debug: false,
+});
